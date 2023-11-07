3d001aa95fd5c0f468b7b62463d2d000d707f167
dolidev2 <dolidev2.0@gmail.com>
2023-11-07 14:02:15 +0000

    test 1 deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "Deployment started ..."
 (php artisan down) || true
 
 # Pull the latest version of the app
-cd /var/www/html/bourse
+cd /var/www/html/fret
 git pull
 
 # allow composer for root
